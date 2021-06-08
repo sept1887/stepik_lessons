6f3af67dc9a2b5cd2317bab43b4b4255da6cdf77
sept1887 <secrets-inside@yandex.ru>
2021-06-08 22:16:17 +0700

    added module_4

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 def test_login():
-    #Data
+    # Data
     main_page_link = "http://selenium1py.pythonanywhere.com/ru/"
     user_name_value = "secrets-inside@yandex.ru"
     password_value = "Ussur692527"
@@ -29,10 +29,10 @@ def test_login():
         password.send_keys(password_value)
         submit_btn = browser.find_element(By.CSS_SELECTOR, submit_btn_locator)
         submit_btn.click()
-        success_msg = browser.find_element(By.CLASS_NAME, success_msg_locator)
-        success_msg = success_msg.text
 
         #Assert
+        success_msg = browser.find_element(By.CLASS_NAME, success_msg_locator)
+        success_msg = success_msg.text
         assert success_msg == exp_success_msg, \
             f'Expected message is "{exp_success_msg}", but actual message is "{success_msg}"'
 
---
+++ module_4/part_1/test_items.py
@@ -0,0 +1,35 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Data
+main_page_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+btn_locator = 'button.btn-add-to-basket'
+lang_locator = 'no-js'
+
+exp_add_to_cart_btn_txt = {
+    'ru': 'Добавить в корзину',
+    'en-gb': 'Add to basket',
+    'es': 'Añadir al carrito',
+    'fr': 'Ajouter au panier'
+}
+
+
+def test_add_to_cart_btn_lang(browser, language):
+    try:
+        # Arrange
+        browser.get(main_page_link)
+
+        # Act
+        add_to_cart_btn = browser.find_element(By.CSS_SELECTOR, btn_locator)
+        page_lang = browser.find_element(By.CLASS_NAME, lang_locator).get_attribute('lang')
+
+        # Assert
+        assert page_lang == language, \
+            f'Expected page language is "{language}", but actual page language is "{page_lang}"'
+        assert add_to_cart_btn.text in exp_add_to_cart_btn_txt[language], \
+            f'Expected button text is "{exp_add_to_cart_btn_txt[language]}, but actual button text is "{add_to_cart_btn.text}"'
+
+    finally:
+        browser.quit()
+
