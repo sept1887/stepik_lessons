2c5260b2d9c6078503d64de94d754b3e25d927c4
sept1887 <secrets-inside@yandex.ru>
2021-06-24 16:11:25 +0700

    optimized links, added AAA

--- module_5/test_main_page.py
+++ module_5/test_main_page.py
@@ -4,34 +4,39 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 @pytest.mark.login_guest
 class TestLoginFromMainPage:
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
+        # Arrange
         main_page = MainPage(browser, link)
         main_page.open()
+        # Act
         main_page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
+        # Assert
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
+        # Arrange
         main_page = MainPage(browser, link)
+        # Act
         main_page.open()
+        # Assert
         main_page.should_be_login_link()
 
 
 @pytest.mark.basket_guest
 class TestBasketFromMainPage:
     def test_guest_cant_see_product_in_basket_opened_from_main_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
+        # Arrange
         main_page = MainPage(browser, link)
         main_page.open()
+        # Act
         main_page.go_to_basket_page()
         basket_page = BasketPage(browser, browser.current_url)
+        # Assert
         basket_page.should_not_be_items_in_basket()
         basket_page.should_be_empty_basket_msg()
-
-
-
-
